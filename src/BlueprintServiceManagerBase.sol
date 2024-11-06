04886ccb8fc8fb6e7d9178607af084f2c45b1c02
Drew Stone <drewstone329@gmail.com>
2024-11-06 17:32:42 +0700

    chore: fmt

--- src/BlueprintServiceManagerBase.sol
+++ src/BlueprintServiceManagerBase.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.19;
 import "src/Permissions.sol";
 import "src/IBlueprintServiceManager.sol";
 
-/**fo
+/**
+ * fo
  * @title BlueprintServiceManagerBase
  * @dev This contract acts as a manager for the lifecycle of a Blueprint Instance,
  * facilitating various stages such as registration, service requests, job execution,
